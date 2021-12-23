8f1e2b9c65d3893eb0e3216d30ebe882bbb2a308
Martin Rotelli <martin_rot@hotmail.com>
2021-12-22 21:24:45 -0300

    Clase 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+/* import MiModulo from './MiModulo' */
+import { MiModulo } from './MiModulo'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          BIENVENIDOS - REACT JS 2021
+        </p>      
+
+        <section>
+          <h2>
+            holA!
+          </h2>
+        </section>
+
       </header>
+
+        <section>
+          <h3>
+            hola !
+          </h3>
+        </section>
+
+        <MiModulo/>   
+
     </div>
   );
 }
---
+++ src/MiModulo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const MiModulo = () => {
+
+    return (
+        <div>
+            <h2>Hola desde mi modulo</h2>
+            <h3>HOLA!</h3>
+        </div>
+    )
+}
