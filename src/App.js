d301ef6cfd0e2ae172856f60b573b12d333bd5d5
Martin Rotelli <martin_rot@hotmail.com>
2022-01-09 19:39:56 -0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,44 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
-/* import MiModulo from './MiModulo' */
-import { MiModulo } from './MiModulo'
-import { NavBar } from '../src/components/NavBar'
+import { MiModulo } from './MiModulo';
+import { NavBar } from '../src/components/NavBar';
 
 function App() {
   return (
-    <div className="App">
 
-      <NavBar/>
+  
+<div className="App">
+ 
+  <NavBar/>
 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <p>
-          BIENVENIDOS - REACT JS 2021
-        </p>      
+  <div className="container">
 
-        <section>
-          <h2>
-            holA!
-          </h2>
-        </section>
+    <header className="App-header">
 
-      </header>
+      <p>
+        BIENVENIDOS - REACT JS 2021
+      </p>      
 
-        <section>
-          <h3>
-            hola !
-          </h3>
-        </section>
+      <section>
+        <h2>
+          holA!
+        </h2>
+      </section>
 
-        <MiModulo/>   
+    </header>
+  </div>                                 
 
-    </div>
+  <section>
+    <h3>
+      hola !
+    </h3>
+  </section>
+
+  <MiModulo/>   
+
+</div>
+
   );
 }
 
 console.log("HGFGOKDFOGHK");
-
-
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,40 @@
 import React from 'react'
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 
 console.log("Hola desde NavBar");
 
 export const NavBar = () => {
     
     return (
-        <div>
-            <h1>NavBar</h1>
-        </div>        
+        
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light" fixed="top">
+            <Container>
+                <Navbar.Brand href="home">DEZAPAS</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <NavDropdown title="ZAPATILLAS" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#">Hombre</NavDropdown.Item>
+                            <NavDropdown.Item href="#">Mujer</NavDropdown.Item>
+                            <NavDropdown.Item href="#">Niño</NavDropdown.Item>
+                            <NavDropdown.Item href="#">Todo</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#">Novedades</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="#features">LANZAMIENTO</Nav.Link>
+                        <Nav.Link href="#pricing" className="title-red">SALES</Nav.Link>
+                    </Nav>
+
+                    <Nav>
+                        <Nav.Link href="#">INICIAR SESION</Nav.Link>
+                        <Nav.Link eventKey={2} href="#">
+                        CARRITO
+                        </Nav.Link>
+                    </Nav>
+
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+
     )
 }
